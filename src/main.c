b73bfc3ee3c7627897ef3042f68416b935c46131
thomp901 <garrisonwilliamt@gmail.com>
2025-11-18 20:39:31 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,17 +7,18 @@
 #include "hardware/dma.h"
 
 #include "hardware/i2c.h"
+#include "mpl3115a2_interface.h"
 
 void init_i2c() {
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function();
-    gpio_set_function();
-    gpio_pull_up();
-    gpio_pull_up();
+    //i2c_init(i2c_default, 100 * 1000);
+    //gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    //gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN), GPIO_FUNC_I2C;
+    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 }
 
 int main() {
-    stdio_init_all();
-    sleep_ms(1000);
-    i2c_steup();
+    //stdio_init_all();
+    //sleep_ms(1000);
+    //i2c_steup();
 }
