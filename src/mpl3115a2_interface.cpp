7a73215c50bffd04cb408a9ebb70798c9b1515a7
thomp901 <garrisonwilliamt@gmail.com>
2025-11-18 20:31:21 -0500

    Create mpl3115a2_interface.cpp

---
+++ src/mpl3115a2_interface.cpp
@@ -0,0 +1,27 @@
+//THIS CODE WAS GENERATED USING AI
+//THIS IS INSTANTIATING THE C++ Library
+//DEFINING THE C Functions
+
+// src/mpl3115a2_interface.cpp
+
+#include <Adafruit_MPL3115A2.h>
+#include "mpl3115a2_interface.h"
+
+// Instantiate the static C++ sensor object
+static Adafruit_MPL3115A2 baro;
+
+// Ensure C-style linking for the function DEFINITIONS
+extern "C" {
+    
+    int32_t baro_init(void) {
+        return baro.begin();
+    }
+
+    float baro_get_pressure_pascal(void) {
+        return baro.getPressure();
+    }
+    
+    float baro_get_altitude_meters(void) {
+        return baro.getAltitude();
+    }
+}
