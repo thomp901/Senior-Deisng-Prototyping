0e6aaaeb934310929fbd2f7b0b5c9e864e025cfb
thomp901 <garrisonwilliamt@gmail.com>
2025-11-19 10:44:18 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,15 +10,21 @@
 #include "mpl3115a2_interface.h"
 
 void init_i2c() {
-    //i2c_init(i2c_default, 100 * 1000);
-    //gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    //gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN), GPIO_FUNC_I2C;
-    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 }
 
 int main() {
-    //stdio_init_all();
-    //sleep_ms(1000);
-    //i2c_steup();
+    stdio_init_all();
+
+    init_i2c();
+
+    int32_t pressure_reading = baro_get_pressure_pascal();
+    int32_t temperature_reading = baro_get_altitude_meters();
+
+    float temperature = baro_getTemperature();
+    
 }
